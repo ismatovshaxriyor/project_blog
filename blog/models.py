e802db0c0334026651b0f71665a78a6c5fd07a29
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-05-17 00:05:36 +0500

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique_for_date="publish")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-published')
+
+    def __str__(self):
+        return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
+
+posts = Post.objects.all()
+p_posts = Post.published.all()
