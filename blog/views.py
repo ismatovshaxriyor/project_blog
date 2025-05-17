3eacb47ae049808244f2c5cd03de2eb48a826d01
shaxa-dev-000000 <shaxriyorismatov14@gmail.com>
2025-05-17 15:45:33 +0500

    Initial commit

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView
 from .models import Post
+from .forms import CommentForm
 
 # Create your views here.
 class PostListView(ListView):
@@ -11,4 +12,22 @@ class PostListView(ListView):
 
 def post_detail(request, year, month, day, slug):
     post = get_object_or_404(Post, slug=slug, status='published', publish__year=year, publish__month=month, publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, 'blog/post/detail.html', {
+        'post': post,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form
+    })
